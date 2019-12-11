068773a52ea2105c028948d540b5e1ed9e5792ef
ZPistovcakova <56343625+ZPistovcakova@users.noreply.github.com>
2019-12-11 12:21:50 +0100

    Add files via upload

--- klik.py
+++ klik.py
@@ -17,6 +17,11 @@ def pohyb(event):
     x, y = event.x, event.y
     r = 10
     canvas.create_oval(x-r, y-r, x+r, y+r, fill = "blue", outline = "blue")
+    a = X_MAX - x
+    canvas.create_oval(a-r, y-r, a+r, y+r, fill = "red", outline = "red")
+    b = Y_MAX - y
+    canvas.create_oval(x-r, b-r, x+r, b+r, fill = "green", outline = "green")
+    canvas.create_oval(a-r, b-r, a+r, b+r, fill = "yellow", outline = "yellow")
 
 #canvas.bind('<Button-1>', gula)    
 canvas.bind('<Motion>', pohyb)
